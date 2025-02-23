9ca6ca7aba8f8ef6e1c2aad96eb5ea13f92353ee
meoweird <meoweird.dev@gmail.com>
2025-02-23 17:32:45 +0700

    feat: Set up MongoDb connection and update requirements. Customize customer

--- customer/models.py
+++ customer/models.py
@@ -2,21 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-# class Customer(AbstractUser):
-#     username = models.CharField(max_length=200, unique=True)
-#     email = models.EmailField(max_length=200, unique=True)
-#     password = models.CharField(max_length=200)
-#     first_name = models.CharField(max_length=200)
-#     last_name = models.CharField(max_length=200)
-#     phone = models.CharField(max_length=200)
-#     address = models.TextField()
-#     date_of_birth = models.DateField()
-
-#     def __str__(self):
-#         return self.username
-    
-#     REQUIRED_FIELDS = ['email', 'username', 'password']
-    
-    
-#     class Meta:
-#         app_label = 'customer'
+class Customer(AbstractUser):
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
--- lxdong_proj2/settings.py
+++ lxdong_proj2/settings.py
@@ -198,4 +198,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'customer.Customer'
+MONGO_URI = os.getenv("MONGO_URI")
+
 
